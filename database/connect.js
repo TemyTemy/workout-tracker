514ca3bee42f6aadbc757ac8a796a75181c05636
Temi <tayolanre2000@yahoo.com>
2021-06-27 16:04:51 +0100

    UPDATE: deplying to Heroku

--- database/connect.js
+++ database/connect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 function connect() {
-    const connection = mongoose.connect(process.env.MONGODB, {
+    const connection = mongoose.connect(process.env.MONGODB || 'mongodb://localhost:27017/?Workout', {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true,
